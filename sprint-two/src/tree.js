8bb70283c2498dad5fa2a8ebd4e2ad625321e436
nvodoor <nvodoor@gmail.com>
2017-02-09 14:06:26 -0800

    First two tests passed. Fixed test.

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -2,8 +2,10 @@ var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
 
+  _.extend(newTree, treeMethods);
+
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];  // fix me
 
   return newTree;
 };
@@ -11,6 +13,8 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  console.log(this)
+  this.children.push(value);
 };
 
 treeMethods.contains = function(target) {
