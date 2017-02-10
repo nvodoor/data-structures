262a8d047907d9fe00038840c43a54c73cc3f4f5
nvodoor <nvodoor@gmail.com>
2017-02-10 10:54:53 -0800

    Laid out pseudocode after whiteboarding bst.insert

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,5 +1,7 @@
 var BinarySearchTree = function(value) {
   this.value = value;
+  this.left = null;
+  this.right = null;
 };
 
 BinarySearchTree.prototype.left = function left() {
@@ -10,7 +12,25 @@ BinarySearchTree.prototype.right = function right() {
   return this.right;
 };
 
-BinarySearchTree.prototype.insert = function insert(nodeValue, tree) {
+BinarySearchTree.prototype.insert = function insert(nodeValue) {
+  var node = new Node(nodeValue);
+
+  // set current to this.value
+  // while true
+    // if node.value < current.value
+      // if current.left is null
+        // current.left is node
+        // return
+      // else 
+        // current is current.left
+
+    // if node.value > current.value
+      // if current.right is null
+        // current.right is node
+          // return 
+      // else 
+        // current = current.right
+
 };
 
 BinarySearchTree.prototype.contains = function contains() {
@@ -25,7 +45,7 @@ BinarySearchTree.prototype.Node = function Node(value) {
   this.value = value;
   this.left = null;
   this.right = null;
-}
+};
 
 
 /*
