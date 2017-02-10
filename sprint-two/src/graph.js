a39c02007ce65c39dc429a913208dec57d0829b5
nvodoor <nvodoor@gmail.com>
2017-02-09 16:38:58 -0800

    Complete removeNode method.

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -15,23 +15,23 @@ Graph.prototype.addNode = function(value) {
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(target) {
 
-  var exists = false;
-  var nodeSearch = function nodeSearch (node) {
-
+  return this.nodeList.reduce(function(acc, node) {
     if (node.value === target) {
-      return exists = true;
-    } else if (node.nodeList) {
-      node.nodeList.forEach(function(miniNode) {
-        nodeSearch(miniNode);
-      });
+      acc = true;
     }
-  };
-  nodeSearch(this);
-  return exists;
+    return acc;
+  }, false);
 };
 
 // Removes a node from the graph.
-Graph.prototype.removeNode = function(node) {
+Graph.prototype.removeNode = function(nodeValue) {
+
+  return this.nodeList.reduce(function(acc, eachNode, index) {
+    if (nodeValue === eachNode.value) {
+      acc.splice(index, 1);
+    }
+    return acc;
+  }, this.nodeList);
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
@@ -53,9 +53,23 @@ Graph.prototype.forEachNode = function(cb) {
 Graph.prototype.generateNode = function(value) {
   this.value = value;
   this.edgeList = [];
-}
+};
 /*
  * Complexity: What is the time complexity of the above functions?
  */
 
 
+
+  // var exists = false;
+  // var nodeSearch = function nodeSearch (node) {
+
+  //   if (node.value === target) {
+  //     return exists = true;
+  //   } else if (node.nodeList) {
+  //     node.nodeList.forEach(function(miniNode) {
+  //       nodeSearch(miniNode);
+  //     });
+  //   }
+  // };
+  // nodeSearch(this);
+  // return exists;
