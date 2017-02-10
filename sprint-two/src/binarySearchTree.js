ee5b41aba12751af9bf11b41054bc36c4a70795b
Jason Chambers <jas.o.chambers@gmail.com>
2017-02-09 22:21:22 -0800

    Set up layout for binarySearchTree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,6 +1,26 @@
 var BinarySearchTree = function(value) {
 };
 
+BinarySearchTree.prototype.left = function left() {
+
+};
+
+BinarySearchTree.prototype.right = function right() {
+
+};
+
+BinarySearchTree.prototype.insert = function insert() {
+
+};
+
+BinarySearchTree.prototype.contains = function contains() {
+
+};
+
+BinarySearchTree.prototype.depthFirstLog = function depthFirstLog() {
+
+};
+
 
 /*
  * Complexity: What is the time complexity of the above functions?
