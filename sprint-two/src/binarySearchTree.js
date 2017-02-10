ae495d580f18d2c4e4ec605ce4080664fbe20809
Jason Chambers <jas.o.chambers@gmail.com>
2017-02-10 00:16:49 -0800

    pass first binarySearchTree test.

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,16 +1,16 @@
 var BinarySearchTree = function(value) {
+  this.value = value;
 };
 
 BinarySearchTree.prototype.left = function left() {
-
+  return this.left;
 };
 
 BinarySearchTree.prototype.right = function right() {
-
+  return this.right;
 };
 
-BinarySearchTree.prototype.insert = function insert() {
-
+BinarySearchTree.prototype.insert = function insert(nodeValue, tree) {
 };
 
 BinarySearchTree.prototype.contains = function contains() {
@@ -20,6 +20,12 @@ BinarySearchTree.prototype.contains = function contains() {
 BinarySearchTree.prototype.depthFirstLog = function depthFirstLog() {
 
 };
+
+BinarySearchTree.prototype.Node = function Node(value) {
+  this.value = value;
+  this.left = null;
+  this.right = null;
+}
 
 
 /*
