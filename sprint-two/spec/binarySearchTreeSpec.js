5569ac4420db134f4d7ef81f54ce8438f44ac3d0
Jason Chambers <jas.o.chambers@gmail.com>
2017-02-09 22:35:39 -0800

    Pass fist binarySearchTree test

--- sprint-two/spec/binarySearchTreeSpec.js
+++ sprint-two/spec/binarySearchTreeSpec.js
@@ -2,7 +2,7 @@ describe('binarySearchTree', function() {
   var binarySearchTree;
 
   beforeEach(function() {
-    binarySearchTree = BinarySearchTree(5);
+    binarySearchTree = new BinarySearchTree;
   });
 
   it('should have methods named "insert", "contains", and "depthFirstLog', function() {
