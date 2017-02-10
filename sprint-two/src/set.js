72b73a52ea36ff1766397669e7f6725b4c2f6871
nvodoor <nvodoor@gmail.com>
2017-02-09 19:51:24 -0800

    Set passes all tests - set.js

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,33 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = {};
+  set.length = 0;
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  if (!this[item]) {
+    this.length++;
+  }
+  this[item] = item;
 };
 
 setPrototype.contains = function(item) {
+  if (this[item]) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 setPrototype.remove = function(item) {
+  this.length--;
+  delete this[item];
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ 0 (1)
  */
